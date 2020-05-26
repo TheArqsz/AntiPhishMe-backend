2e81c8fcf7c085adc1b33d42532b9d799c2f3747
TheArqsz <arek.maruszczak@gmail.com>
2020-05-26 20:49:56 +0200

    Fixed sh

--- scripts/upload_allure_results.sh
+++ scripts/upload_allure_results.sh
@@ -3,8 +3,11 @@
 echo "Creating ssh key"
 mkdir -p ~/.ssh \
 && ssh_key=`echo "${SSH_ALLURE_KEY}" | base64 -d` \
-&& echo -e "${ssh_key}" | sed -r 's/_/\n/g' > ~/.ssh/allure_id_rsa \
+&& echo "${ssh_key}" | sed -r 's/_/\n/g' > ~/.ssh/allure_id_rsa \
 && chmod og-rwx ~/.ssh/allure_id_rsa
+
+echo "Clear remote"
+ssh -i ~/.ssh/allure_id_rsa -oStrictHostKeyChecking=no -oBatchMode=yes allure@${SSH_ALLURE_HOST} "rm -rf ${SSH_ALLURE_REMOTE_PATH}/*"
 
 echo "Uploading files"
 rsync -Pav -e 'ssh -i ~/.ssh/allure_id_rsa -oStrictHostKeyChecking=no -oBatchMode=yes' ../antiphishme/tests/results/* "allure@${SSH_ALLURE_HOST}:${SSH_ALLURE_REMOTE_PATH}/"
