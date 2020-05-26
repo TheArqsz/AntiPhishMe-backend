b1542e0bb0c9a6c99319c4e83c07e6421c8c25ee
TheArqsz <arek.maruszczak@gmail.com>
2020-05-26 20:40:33 +0200

    Changed way of ssh

--- scripts/upload_allure_results.sh
+++ scripts/upload_allure_results.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.ssh \
 && chmod og-rwx ~/.ssh/allure_id_rsa
 
 echo "Uploading files"
-rsync -Pav -e 'ssh -oStrictHostKeyChecking=no -i ~/.ssh/allure_id_rsa' ../antiphishme/tests/results/* "allure@${SSH_ALLURE_HOST}:${SSH_ALLURE_REMOTE_PATH}/"
+rsync -Pav -e 'ssh -i ~/.ssh/allure_id_rsa -oStrictHostKeyChecking=no' ../antiphishme/tests/results/* "allure@${SSH_ALLURE_HOST}:${SSH_ALLURE_REMOTE_PATH}/"
 
 echo "Cleaning ssh key"
 rm -f ~/.ssh/allure_id_rsa
