92be524101b0272615506630c6d0c01d0fb7711c
TheArqsz <arek.maruszczak@gmail.com>
2020-05-26 20:34:26 +0200

    Added sshkey

--- scripts/upload_allure_results.sh
+++ scripts/upload_allure_results.sh
@@ -2,11 +2,12 @@
 
 echo "Creating ssh key"
 mkdir -p ~/.ssh \
-&& echo -e "${SSH_ALLURE_KEY}" | sed -r 's/_/\n/g' > ~/.ssh/allure_id_rsa \
+&& ssh_key=`echo "${SSH_ALLURE_KEY}" | base64 -d` \
+&& echo -e "${ssh_key}" | sed -r 's/_/\n/g' > ~/.ssh/allure_id_rsa \
 && chmod og-rwx ~/.ssh/allure_id_rsa
 
 echo "Uploading files"
-rsync -Pav -e 'ssh -i ~/.ssh/allure_id_rsa' ../antiphishme/tests/results/* "allure@${SSH_ALLURE_HOST}:${SSH_ALLURE_REMOTE_PATH}/"
+rsync -Pav -e 'ssh -oStrictHostKeyChecking=no -i ~/.ssh/allure_id_rsa' ../antiphishme/tests/results/* "allure@${SSH_ALLURE_HOST}:${SSH_ALLURE_REMOTE_PATH}/"
 
 echo "Cleaning ssh key"
 rm -f ~/.ssh/allure_id_rsa
