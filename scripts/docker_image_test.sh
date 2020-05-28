fd0f62fee295d9af48f5180c820f75ca52f6bfa3
TheArqsz <arek.maruszczak@gmail.com>
2020-05-29 00:46:36 +0200

    Added test for docker

---
+++ scripts/docker_image_test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "Checking if image $LOCAL_DOCKER_IMAGE is operational"
+is_present=`docker ps | grep -c $LOCAL_DOCKER_IMAGE`
+
+if [ $is_present -eq 1 ]
+then
+    echo "Image \"$LOCAL_DOCKER_IMAGE\" exists - checking stability"
+else
+    echo "Image \"$LOCAL_DOCKER_IMAGE\" is not working - exiting"
+    export LOCAL_DOCKER_IMAGE_STATUS="not working"
+    exit 1
+fi
+
+response=$(curl --write-out %{http_code} --silent --output /dev/null $LOCAL_DOCKER_SERVER)
+if [ $response -eq 404 ]
+then
+    echo "Image \"$LOCAL_DOCKER_IMAGE\" is working"
+    export LOCAL_DOCKER_IMAGE_STATUS="working"
+    exit 0
+else    
+    echo "Image \"$LOCAL_DOCKER_IMAGE\" is not working"
+    export LOCAL_DOCKER_IMAGE_STATUS="not working"
+    exit 1
+fi
